a0c8c0b3b8cdb31195d03f5f333000d730ff0035
svalencia <svalencia@greenflex.com>
2018-10-06 10:52:07 +0200

    Init ejected app

--- packages/test-next-ejected/src/App.test.js
+++ packages/test-next-ejected/src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders app with react-script 2 without crashing', () => {
+it('renders ejected app with react-script 2 without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
